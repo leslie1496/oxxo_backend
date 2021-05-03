55bcd2fdcd4fbb13af9c3dfa027d50e9fed7b023
unknown <leslieaylins@gmail.com>
2021-05-03 16:27:56 -0500

    commit inicial

---
+++ src/main/java/mx/com/oxxo/OxxoBackendApplication.java
@@ -0,0 +1,13 @@
+package mx.com.oxxo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxxoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxxoBackendApplication.class, args);
+	}
+
+}
